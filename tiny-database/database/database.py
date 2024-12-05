bb2c224500c6fe44eaf358d778d88ac5e829afa0
Kristina Puzina <puzina.kristina@yandex.ru>
2024-12-05 20:54:43 +0300

    task 3

--- tiny-database/database/database.py
+++ tiny-database/database/database.py
@@ -1,10 +1,11 @@
-from abc import ABC, abstractmethod
 import csv
 import os
+from abc import ABC, abstractmethod
 
 
 class SingletonMeta(type):
-    """ Синглтон метакласс для Database. """
+    """Синглтон метакласс для Database."""
+
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
@@ -14,7 +15,7 @@ class SingletonMeta(type):
 
 
 class Database(metaclass=SingletonMeta):
-    """ Класс-синглтон базы данных с таблицами, хранящимися в файлах. """
+    """Класс-синглтон базы данных с таблицами, хранящимися в файлах."""
 
     def __init__(self):
         self.tables = {}
@@ -40,10 +41,14 @@ class Database(metaclass=SingletonMeta):
             index = tables.index(None)
             raise ValueError(f"Table {table_names[index]} does not exist.")
 
-        joined_data = self.__join_two_tables(tables[0].data, tables[1].data, join_attrs[0])
+        joined_data = self.__join_two_tables(
+            tables[0].data, tables[1].data, join_attrs[0]
+        )
 
         for i in range(2, len(tables)):
-            joined_data = self.__join_two_tables(joined_data, tables[i].data, join_attrs[i - 1])
+            joined_data = self.__join_two_tables(
+                joined_data, tables[i].data, join_attrs[i - 1]
+            )
 
         return joined_data
 
@@ -65,7 +70,9 @@ class Database(metaclass=SingletonMeta):
             raise ValueError(f"Table {table_name} does not exist.")
 
         if column not in table.ATTRS:
-            raise ValueError(f"There is no such {column} column in the table {table_name}")
+            raise ValueError(
+                f"There is no such {column} column in the table {table_name}"
+            )
 
         values = [int(row[column]) for row in table.data]
 
@@ -73,48 +80,58 @@ class Database(metaclass=SingletonMeta):
 
 
 class Table(ABC):
-    """ Абстрактный базовый класс для таблиц с вводом/выводом файлов CSV. """
+    """Абстрактный базовый класс для таблиц с вводом/выводом файлов CSV."""
 
     @abstractmethod
     def insert(self, data):
-        pass
+        pass  # pragma: no cover
 
     @abstractmethod
     def select(self, *args):
-        pass
+        pass  # pragma: no cover
 
 
 class EmployeeTable(Table):
-    """ Таблица сотрудников с методами ввода-вывода из файла CSV. """
-    ATTRS = ('employee_id', 'name', 'age', 'salary', 'department_id')
-    FILE_PATH = 'employee_table.csv'
+    """Таблица сотрудников с методами ввода-вывода из файла CSV."""
+
+    ATTRS = ("employee_id", "name", "age", "salary", "department_id")
+    FILE_PATH = "employee_table.csv"
 
     def __init__(self):
         self.data = []
         self.load()  # Подгружаем из CSV-файла сразу при инициализации
 
     def insert(self, data):
-        entry = dict(zip(self.ATTRS, data.split(',')))
+        entry = dict(zip(self.ATTRS, data.split(",")))
 
         for row in self.data:
-            if (row["employee_id"], row["department_id"]) == (entry["employee_id"], entry["department_id"]):
-                raise ValueError("The pair (employee_id, department_id) must be unique")
+            if (row["employee_id"], row["department_id"]) == (
+                entry["employee_id"],
+                entry["department_id"],
+            ):
+                raise ValueError(
+                    "The pair (employee_id, department_id) must be unique"
+                )
 
         self.data.append(entry)
         self.save()
 
     def select(self, start_id, end_id):
-        return [entry for entry in self.data if start_id <= int(entry['employee_id']) <= end_id]
+        return [
+            entry
+            for entry in self.data
+            if start_id <= int(entry["employee_id"]) <= end_id
+        ]
 
     def save(self):
-        with open(self.FILE_PATH, 'w', newline='') as f:
+        with open(self.FILE_PATH, "w", newline="") as f:
             writer = csv.DictWriter(f, fieldnames=self.ATTRS)
             writer.writeheader()
             writer.writerows(self.data)
 
     def load(self):
         if os.path.exists(self.FILE_PATH):
-            with open(self.FILE_PATH, 'r') as f:
+            with open(self.FILE_PATH, "r") as f:
                 reader = csv.DictReader(f)
                 self.data = [row for row in reader]
         else:
@@ -122,19 +139,24 @@ class EmployeeTable(Table):
 
 
 class DepartmentTable(Table):
-    """ Таблица подразделенией с вводлм-выводом в/из CSV файла. """
-    ATTRS = ('department_id', 'department_name', 'address_id')
-    FILE_PATH = 'department_table.csv'
+    """Таблица подразделенией с вводлм-выводом в/из CSV файла."""
+
+    ATTRS = ("department_id", "department_name", "address_id")
+    FILE_PATH = "department_table.csv"
 
     def __init__(self):
         self.data = []
         self.load()
 
     def select(self, department_name):
-        return [entry for entry in self.data if department_name == entry['department_name']]
+        return [
+            entry
+            for entry in self.data
+            if department_name == entry["department_name"]
+        ]
 
     def insert(self, data):
-        entry = dict(zip(self.ATTRS, data.split(',')))
+        entry = dict(zip(self.ATTRS, data.split(",")))
 
         for row in self.data:
             if row["department_id"] == entry["department_id"]:
@@ -144,14 +166,14 @@ class DepartmentTable(Table):
         self.save()
 
     def save(self):
-        with open(self.FILE_PATH, 'w', newline='') as f:
+        with open(self.FILE_PATH, "w", newline="") as f:
             writer = csv.DictWriter(f, fieldnames=self.ATTRS)
             writer.writeheader()
             writer.writerows(self.data)
 
     def load(self):
         if os.path.exists(self.FILE_PATH):
-            with open(self.FILE_PATH, 'r') as f:
+            with open(self.FILE_PATH, "r") as f:
                 reader = csv.DictReader(f)
                 self.data = [row for row in reader]
         else:
@@ -159,19 +181,24 @@ class DepartmentTable(Table):
 
 
 class AddressTable(Table):
-    """ Таблица адресов с вводом-выводом в/из CSV файла. """
-    ATTRS = ('address_id', 'city', 'street', 'house_number')
-    FILE_PATH = 'address_table.csv'
+    """Таблица адресов с вводом-выводом в/из CSV файла."""
+
+    ATTRS = ("address_id", "city", "street", "house_number")
+    FILE_PATH = "address_table.csv"
 
     def __init__(self):
         self.data = []
         self.load()
 
     def select(self, start_id, end_id):
-        return [entry for entry in self.data if start_id <= int(entry['address_id']) <= end_id]
+        return [
+            entry
+            for entry in self.data
+            if start_id <= int(entry["address_id"]) <= end_id
+        ]
 
     def insert(self, data):
-        entry = dict(zip(self.ATTRS, data.split(',')))
+        entry = dict(zip(self.ATTRS, data.split(",")))
 
         for row in self.data:
             if row["address_id"] == entry["address_id"]:
@@ -181,14 +208,14 @@ class AddressTable(Table):
         self.save()
 
     def save(self):
-        with open(self.FILE_PATH, 'w', newline='') as f:
+        with open(self.FILE_PATH, "w", newline="") as f:
             writer = csv.DictWriter(f, fieldnames=self.ATTRS)
             writer.writeheader()
             writer.writerows(self.data)
 
     def load(self):
         if os.path.exists(self.FILE_PATH):
-            with open(self.FILE_PATH, 'r') as f:
+            with open(self.FILE_PATH, "r") as f:
                 reader = csv.DictReader(f)
                 self.data = [row for row in reader]
         else:
